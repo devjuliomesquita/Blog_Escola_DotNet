67c7b278240e0be6aabcc60d55aeb59b45f533d5
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-03-27 03:40:13 -0300

    Criado as models Page, Post e ApplicationUser

---
+++ Blog_Escola/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Blog_Escola.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        //Relacionamentos
+        public List<Post>? Posts { get; set; }
+    }
+}
---
+++ Blog_Escola/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace Blog_Escola.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        //Relacionamentos
+        public string? ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string? Description { get; set; }
+        public string? Slug { get; set; }
+    }
+}
