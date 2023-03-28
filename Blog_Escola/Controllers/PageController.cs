2d1bc887878cbf9c392a5a4b2a2d9604b721fd95
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-03-28 01:48:47 -0300

    Estilizado as abas de Home, About, Contact, Privacy. Criado a controller Page

---
+++ Blog_Escola/Controllers/PageController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_Escola.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
