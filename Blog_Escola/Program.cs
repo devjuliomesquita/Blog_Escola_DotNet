f8191b497eb64d951954e509219b35f0727b7bd1
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-03-28 02:36:59 -0300

    Algumas alterações de Header e uma nova area de admin

--- Blog_Escola/Program.cs
+++ Blog_Escola/Program.cs
@@ -39,7 +39,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
