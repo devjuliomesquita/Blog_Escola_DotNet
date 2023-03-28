c026d44fa57cb6a2e356b281098f70b95041ca7b
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-03-28 03:35:02 -0300

    Estilizado uma parte do administrador

--- Blog_Escola/Program.cs
+++ Blog_Escola/Program.cs
@@ -38,8 +38,11 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
+    name: "area",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
