1c9e3385c638d3de69758a5c83d5b467acf962f7
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-03-31 13:53:14 -0300

    Controller de Craição de Usuários OK

--- Blog_Escola/Areas/Admin/Controllers/UserController.cs
+++ Blog_Escola/Areas/Admin/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using AspNetCoreHero.ToastNotification.Abstractions;
 using Blog_Escola.Models;
+using Blog_Escola.Utilites;
 using Blog_Escola.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -43,6 +44,52 @@ namespace Blog_Escola.Areas.Admin.Controllers
         public IActionResult Register()
         {
             return View( new RegisterVM());
+        }
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            //Checar se o formulário é válido
+            if(!ModelState.IsValid) return View(registerVM);
+
+            //Checando se já existe email ou usuário
+            var checkUserByEmail = await _userManager.FindByEmailAsync(registerVM.Email);
+            if(checkUserByEmail != null)
+            {
+                _iNotyfService.Error("Este e-mail já existe.");
+                return View(registerVM);
+            }
+            var checkUserByUsername = await _userManager.FindByNameAsync(registerVM.UserName);
+            if(checkUserByUsername != null)
+            {
+                _iNotyfService.Error("Este usuário já existe.");
+                return View(registerVM);
+            }
+            //Criando o objeto ApplicatiomUser
+            var applicationUser = new ApplicationUser()
+            {
+                Email = registerVM.Email,
+                UserName = registerVM.UserName,
+                FirstName = registerVM.FirstName,
+                LastName = registerVM.LastName,
+            };
+            var resultUser = await _userManager.CreateAsync(applicationUser, registerVM.Password);
+            //teste do resultado para admin ou não
+            if (resultUser.Succeeded)
+            {
+                if (registerVM.IsAdmin)
+                {
+                    await _userManager.AddToRoleAsync(applicationUser, WebSiteRoles.WebSiteAdmin);
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(applicationUser, WebSiteRoles.WebSiteAuthor);
+                }
+                _iNotyfService.Success("Usuário criado com sucesso.");
+                return RedirectToAction("Index", "User", new {area = "Admin"});
+            }
+            return View(registerVM);
+           
         }
         [HttpGet("Login")]
         public IActionResult Login()
