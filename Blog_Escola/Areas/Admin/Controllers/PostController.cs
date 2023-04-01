b6f10d62056dd57deb75a53999d50dc34e92b50b
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-04-01 16:28:52 -0300

    Controller post Create iniciada

--- Blog_Escola/Areas/Admin/Controllers/PostController.cs
+++ Blog_Escola/Areas/Admin/Controllers/PostController.cs
@@ -1,4 +1,6 @@
-﻿using Blog_Escola.ViewModels;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Blog_Escola.Data;
+using Blog_Escola.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +10,20 @@ namespace Blog_Escola.Areas.Admin.Controllers
     [Authorize]
     public class PostController : Controller
     {
+        //Inicializado o construtor
+        private readonly INotyfService _iNotyfService;
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _environment;
+        public PostController(
+                                INotyfService notyfService,
+                                ApplicationDbContext context,
+                                IWebHostEnvironment environment
+            )
+        {
+            _iNotyfService = notyfService;
+            _context = context;
+            _environment = environment;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,6 +32,27 @@ namespace Blog_Escola.Areas.Admin.Controllers
         public IActionResult Create()
         {
             return View(new CreatePostVM());
+        }
+        //Fazendo o Upload do Post
+        [HttpPost]
+        public async Task<IActionResult> Create(CreatePostVM createPostVM)
+        {
+            if (!ModelState.IsValid) { return View(createPostVM); }
+
+            return View(createPostVM);
+        }
+            //Upload da foto
+        private string UploadImage(IFormFile formFile)
+        {
+            string uniqueFileName = "";
+            var folderPath = Path.Combine(_environment.WebRootPath, "Thumbnails");
+            uniqueFileName = new Guid().ToString() + "_" + formFile.FileName;
+            var filePath = Path.Combine(folderPath, uniqueFileName);
+            using (FileStream fileStream = System.IO.File.Create(filePath))
+            {
+                formFile.CopyTo(fileStream);
+            }
+            return uniqueFileName;
         }
     }
 }
--- Blog_Escola/ViewModels/CreatePostVM.cs
+++ Blog_Escola/ViewModels/CreatePostVM.cs
@@ -1,13 +1,17 @@
 ﻿using Blog_Escola.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Blog_Escola.ViewModels
 {
     public class CreatePostVM
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O Título é Obrigatório.")]
         public string? Title { get; set; }
+        [Required(ErrorMessage = "Uma Pequena Descrição é Obrigatória.")]
         public string? ShortDescription { get; set; }
         public string? ApplicationUserId { get; set; }
+        [Required(ErrorMessage = "A descrição (TEXTO) é Obrigatória.")]
         public string? Description { get; set; }
         public string? ThumbnailUrl { get; set; }
         public IFormFile? Thumbnail { get; set; }
