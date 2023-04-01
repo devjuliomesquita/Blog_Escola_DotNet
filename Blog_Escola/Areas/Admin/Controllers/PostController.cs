be25a4ccf7c1bd3d9ba68cffc87f0e7536f40735
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-03-31 23:40:26 -0300

    Criado a página de Post

--- Blog_Escola/Areas/Admin/Controllers/PostController.cs
+++ Blog_Escola/Areas/Admin/Controllers/PostController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog_Escola.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog_Escola.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class PostController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CreatePostVM());
         }
     }
 }
