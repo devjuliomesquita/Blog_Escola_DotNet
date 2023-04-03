eb5f7a7d1157b0296f230dbf00659b4c5d5151db
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-04-03 04:19:00 -0300

    Criado os métodos da controller Page - páginas de entrada

--- Blog_Escola/Controllers/PageController.cs
+++ Blog_Escola/Controllers/PageController.cs
@@ -1,20 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog_Escola.Data;
+using Blog_Escola.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog_Escola.Controllers
 {
     public class PageController : Controller
     {
-        public IActionResult About()
+        //Feito a instância do banco de dados
+        private readonly ApplicationDbContext _context;
+        public PageController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
-        public IActionResult Contact()
+        public async Task<IActionResult> About()
         {
-            return View();
+            //Encontrar a página correta
+            var page = await _context.Pages!.FirstOrDefaultAsync(p => p.Slug == "about");
+            //Mapear o objeto
+            var viewModel = new PageVM()
+            {
+                Title = page!.Title,
+                ShortDescription = page.ShortDescription,
+                Description = page.Description,
+                ThumbnailUrl = page.ThumbnailUrl
+            };
+            return View(viewModel);
         }
-        public IActionResult Privacy()
+        public async Task<IActionResult> Contact()
         {
-            return View();
+            //Encontrar a página correta
+            var page = await _context.Pages!.FirstOrDefaultAsync(p => p.Slug == "contact");
+            //Mapear o objeto
+            var viewModel = new PageVM()
+            {
+                Title = page!.Title,
+                ShortDescription = page.ShortDescription,
+                Description = page.Description,
+                ThumbnailUrl = page.ThumbnailUrl
+            };
+            return View(viewModel);
+        }
+        public async Task<IActionResult> Privacy()
+        {
+            //Encontrar a página correta
+            var page = await _context.Pages!.FirstOrDefaultAsync(p => p.Slug == "privacy");
+            //Mapear o objeto
+            var viewModel = new PageVM()
+            {
+                Title = page!.Title,
+                ShortDescription = page.ShortDescription,
+                Description = page.Description,
+                ThumbnailUrl = page.ThumbnailUrl
+            };
+            return View(viewModel);
         }
         public IActionResult Teste()
         {
