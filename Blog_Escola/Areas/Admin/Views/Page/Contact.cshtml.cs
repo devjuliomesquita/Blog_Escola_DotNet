c1b0f8c589c9faeabe78b71ce652295095cbc448
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-04-03 03:43:36 -0300

    Criado as Views de Contato e Privacy

---
+++ Blog_Escola/Areas/Admin/Views/Page/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blog_Escola.Areas.Admin.Views.Page
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
