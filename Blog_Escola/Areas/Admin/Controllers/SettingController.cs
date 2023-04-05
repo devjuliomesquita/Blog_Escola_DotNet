665c383acea3f515ab3b2538c3818f236880ff4a
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-04-05 08:01:23 -0300

    Criado a controller Setting

---
+++ Blog_Escola/Areas/Admin/Controllers/SettingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_Escola.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class SettingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
