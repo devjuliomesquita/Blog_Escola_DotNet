4f04c350c4e005fa6859a48ecc8e3499efa54884
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-04-05 07:53:48 -0300

    Adicionado a Model Settings

---
+++ Blog_Escola/Migrations/20230405105249_AddSettings.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog_Escola.Migrations
+{
+    public partial class AddSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SiteName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ShortDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ThumbnailUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GitHub = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LinkedIn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Portifolio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WhatsApp = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Settings");
+        }
+    }
+}
---
+++ Blog_Escola/Models/Setting.cs
@@ -0,0 +1,16 @@
+﻿namespace Blog_Escola.Models
+{
+    public class Setting
+    {
+        public int Id { get; set; }
+        public string? SiteName { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public string? GitHub { get; set; }
+        public string? LinkedIn { get; set; }
+        public string? Gmail { get; set; }
+        public string? Portifolio { get; set; }
+        public string? WhatsApp { get; set; }
+    }
+}
