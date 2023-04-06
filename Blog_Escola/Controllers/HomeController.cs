b8651ff2b9f085471347745f40ae1ca5e05770f0
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-04-05 23:56:50 -0300

    Injetado controller na Index

--- Blog_Escola/Controllers/HomeController.cs
+++ Blog_Escola/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Blog_Escola.Models;
+﻿using Blog_Escola.Data;
+using Blog_Escola.ViewModels;
+using Blog_Escola.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +9,26 @@ namespace Blog_Escola.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var vm = new HomeVM();
+            var setting = _context.Settings!.ToList();
+            vm.Title = setting[0].Title;
+            vm.ShortDescription = setting[0].ShortDescription;
+            vm.ThumbnailUrl = setting[0].ThumbnailUrl;
+            vm.Posts = _context.Posts!.ToList();
+
+            
+
+            return View(vm);
         }
 
         public IActionResult Privacy()
---
+++ Blog_Escola/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using Blog_Escola.Models;
+
+namespace Blog_Escola.ViewModels
+{
+    public class HomeVM
+    {
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public List<Post>? Posts { get; set; }
+    }
+}
