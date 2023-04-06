c12c603c649414fe09cc28a1ba843a010abc6445
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-04-06 01:21:39 -0300

    Criado a controller para mostrar os detalhes do post

---
+++ Blog_Escola/Controllers/BlogController.cs
@@ -0,0 +1,44 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Blog_Escola.Data;
+using Blog_Escola.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog_Escola.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly INotyfService _notyfService;
+        public BlogController(
+            ApplicationDbContext context,
+            INotyfService notyfService)
+        {
+            _context = context;
+            _notyfService = notyfService;
+        }
+        public async Task<IActionResult> Post(string slug)
+        {
+            //Encontrar esse post
+            var post = await _context.Posts!.Include(p => p.ApplicationUser).FirstOrDefaultAsync(p => p.Slug == slug);
+            //Teste
+            if (post == null || slug == "")
+            {
+                _notyfService.Error("Post não encontrado.");
+                return RedirectToAction("Index", "Home", new {area = ""});
+            }
+            //Criar o objeto de Post
+            var vm = new BlogPostVM()
+            {
+                Id = post.Id,
+                Title = post.Title,
+                ShortDescription = post.ShortDescription,
+                AuthorName = post.ApplicationUser.FirstName + " " + post.ApplicationUser.LastName,
+                CreatedAt = post.CreatedAt,
+                ThumbnailUrl = post.ThumbnailUrl,
+                Description = post.Description
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ Blog_Escola/ViewModels/BlogPostVM.cs
@@ -0,0 +1,15 @@
+﻿namespace Blog_Escola.ViewModels
+{
+    public class BlogPostVM
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? Description { get; set; }
+        public string? AuthorName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ThumbnailUrl { get; set; }
+
+
+    }
+}
